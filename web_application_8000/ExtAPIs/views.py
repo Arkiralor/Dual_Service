c31e9fe5e28f7434f27cb8472d030183833a93f1
Arkiralor <prithoo11335@gmail.com>
2022-04-14 23:33:39 +0530

    simplified code.

--- web_application_8000/ExtAPIs/views.py
+++ web_application_8000/ExtAPIs/views.py
@@ -31,7 +31,7 @@ class FindPrimesInRangeView(APIView):
         if not qryset:
             resp = GoAPIHandler.dispatch(task=self.task, query=params)
             resp['requested_by'] = request.user.id
-            if resp['length'] > 8191:
+            if resp.get('length') > 8191:
                 resp['result'] = resp.get('result')[:8190]
                 resp['function'] = resp.get('function') + ' (truncated till [8190])'
             new_qryset = PrimeSerializer(data=resp)
@@ -64,7 +64,7 @@ class FindFactors(APIView):
         if not qryset:
             resp = GoAPIHandler.dispatch(task=self.task, query=params)
             resp['requested_by'] = request.user.id
-            if resp['length'] > 8191:
+            if resp.get('length') > 8191:
                 resp['result'] = resp.get('result')[:8190]
                 resp['function'] = resp.get('function') + ' (truncated till [8190])'
             new_qryset = FactorSerializer(data=resp)
